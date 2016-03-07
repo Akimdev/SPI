182348946d323fb3c3b5702ff5f99a9611efdf1f
Belhaj Othman <belhaj.othman@gmail.com>
2016-03-07 18:07:50 +0100

    Debut service Evaluation

---
+++ src/main/java/fr/univbrest/dosi/spi/service/EvaluationService.java
@@ -0,0 +1,23 @@
+package fr.univbrest.dosi.spi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.univbrest.dosi.spi.bean.Evaluation;
+import fr.univbrest.dosi.spi.dao.EvaluationRepository;
+/**
+ * 
+ * @author Othman
+ *
+ */
+@Service
+public class EvaluationService {
+	@Autowired
+	EvaluationRepository evaRepo;
+	
+	public List<Evaluation> getAllEvaluations(){
+		return (List<Evaluation>) evaRepo.findAll();
+	}
+}
