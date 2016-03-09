8b5149ee01a35e0b3144f446485f3cf0f16e809e
Belhaj Othman <belhaj.othman@gmail.com>
2016-03-09 23:55:38 +0100

    Evaluation effectue avec succes

--- src/test/java/fr/univbrest/dosi/spi/controller/EvaluationControllerTest.java
+++ src/test/java/fr/univbrest/dosi/spi/controller/EvaluationControllerTest.java
@@ -3,25 +3,34 @@ package fr.univbrest.dosi.spi.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.univbrest.dosi.spi.Application;
+import fr.univbrest.dosi.spi.bean.Enseignant;
+import fr.univbrest.dosi.spi.bean.Evaluation;
+import fr.univbrest.dosi.spi.service.EnseignantService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class EvaluationControllerTest {
+	@Autowired
+	EnseignantService ensServ;
+	
 	@Test
 	public void getAllEvaluationsTest() throws ClientProtocolException, IOException {
 
@@ -46,5 +55,35 @@ public class EvaluationControllerTest {
 
 			Assert.assertEquals(200, mockResponse.getStatusLine()
 					.getStatusCode());
+	}
+	
+	@Test
+	public void addEvaluationTest() throws ClientProtocolException, IOException{
+		Evaluation evaluation = new Evaluation();
+		//evaluation.setIdEvaluation(5);
+		Enseignant ens = ensServ.getEnseignant(1);
+		evaluation.setNoEnseignant(ens);
+		evaluation.setNoEvaluation((short)3);
+		evaluation.setEtat("ELA");
+		evaluation.setDesignation("evaluation troix");
+		evaluation.setAnnee("2014-2015");
+		evaluation.setCode_ec("ERP");
+		evaluation.setCode_formation("M2DOSI");
+		evaluation.setCode_ue("PCO");
+		evaluation.setDebutReponse(new Date("18/03/2015"));
+		evaluation.setFinReponse(new Date("22/03/2015"));
+		
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost mockRequestPost = new HttpPost("http://localhost:8090/addEvaluation");
+		final ObjectMapper mapper = new ObjectMapper();
+		final com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+		final String jsonInString = ow.writeValueAsString(evaluation);
+		mockRequestPost.addHeader("content-type", "application/json");
+		mockRequestPost.setEntity(new StringEntity(jsonInString));
+
+		final HttpResponse mockResponse = client.execute(mockRequestPost);
+
+		// Le code retour HTTP doit être un succès (200)
+		Assert.assertEquals(200, mockResponse.getStatusLine().getStatusCode());
 	}
 }
