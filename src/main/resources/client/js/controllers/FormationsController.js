5ddce8a21941e402ecfe18572a7d139ca8f01cee
Belhaj Othman <belhaj.othman@gmail.com>
2016-03-17 16:20:26 +0100

    travail de formation sur nouvelle ihm

--- src/main/resources/client/js/controllers/FormationsController.js
+++ src/main/resources/client/js/controllers/FormationsController.js
@@ -21,6 +21,14 @@
       
       delete: function(codeFormation) { 
     	  return $http.get('http://localhost:8090/formation/delete?codeFormation=' + codeFormation);
+      },
+      
+      getUEsByCodeFormation(codeFormation) {
+    	  return $http.get("http://localhost:8090/getUEsByFormation-"+codeFormation);
+      },
+      
+      getECsByCodeUe(uePK){
+    	  return $http.post("http://localhost:8090/getECByUE",uePK);
       }
     };
   });
@@ -29,15 +37,90 @@
     ['$scope', '$location','$http','$filter', 'formationsFactory',
     function($scope, $location,$http,$filter, formationsFactory){
       // la liste globale des formations
+    	$scope.ue = false;
+    	$scope.ec = false;
     	$scope.refresh = function (){
-    		 var promiseFormation = formationsFactory.all();          
-    	      promiseFormation.success(function(data) {
-    	          $scope.formations = data;
-    	         // $scope.formation.debutAccreditation = $filter('date')(data.debutAccreditation, "dd-MM-yyyy");
-    			 // $scope.formation.finAccreditation = $filter('date')(data.finAccreditation, "dd-MM-yyyy");	
-    	      });
+    		var init = null;
+    		 var promise = formationsFactory.all();          
+    	      promise.success(function(data) {
+    	    	  $scope.formations = data;
+    		      $scope.searchKeywords = '';
+    		      $scope.filteredFormation = [];
+    		      $scope.row = '';
+    		      $scope.select = function(page) {
+    		        var end, start;
+    		        start = (page - 1) * $scope.numPerPage;
+    		        end = start + $scope.numPerPage;
+    		        return $scope.currentPageFormation = $scope.filteredFormation.slice(start, end);
+    		      };
+    		      $scope.onFilterChange = function() {
+    		        $scope.select(1);
+    		        $scope.currentPage = 1;
+    		        return $scope.row = '';
+    		      };
+    		      $scope.onNumPerPageChange = function() {
+    		        $scope.select(1);
+    		        return $scope.currentPage = 1;
+    		      };
+    		      $scope.onOrderChange = function() {
+    		        $scope.select(1);
+    		        return $scope.currentPage = 1;
+    		      };
+    		      $scope.search = function() {
+    		        $scope.filteredFormation = $filter('filter')($scope.formations, $scope.searchKeywords);
+    		        return $scope.onFilterChange();
+    		      };
+    		      $scope.order = function(rowName) {
+    		        if ($scope.row === rowName) {
+    		          return;
+    		        }
+    		        $scope.row = rowName;
+    		        $scope.filteredFormation = $filter('orderBy')($scope.formations, rowName);
+    		        return $scope.onOrderChange();
+    		      };
+    		      $scope.numPerPageOpt = [3, 5, 10, 20];
+    		      $scope.numPerPage = $scope.numPerPageOpt[2];
+    		      $scope.currentPage = 1;
+    		      $scope.currentPageFormation = [];
+    		      init = function() {
+    		        $scope.search();
+    		        return $scope.select($scope.currentPage);
+    		      };
+    		      return init();
+    		  }
+    		)
+        
+    		.error(function(data) {
+    			 $scope.error = 'unable to get the poneys';
+    		  }
+    		);
+    	      
+    	   
     	}
-     
+    	//Affiche les informations sur les unités d'enseignement concernant une formation
+    	$scope.afficheUE = function(codeFormation){
+	    	 var promiseUE = formationsFactory.getUEsByCodeFormation(codeFormation);
+	    	    promiseUE.success(function(data){
+	    	    	$scope.ues = data;
+	    	    	$scope.ue = true;
+	    	    })
+	    	    .error(function(data){
+	    	    	$scope.error = 'unable to get the poneys';
+	    	    });
+	    	}   
+	    	
+    	//Affiche les informations sur les elements constitutifs concernant une formation
+	    	$scope.afficheEC = function(uePK){
+	    		console.log(uePK);
+		    	 var promiseEC = formationsFactory.getECsByCodeUe(uePK);
+		    	    promiseEC.success(function(data){
+		    	    	$scope.ecs = data;
+		    	    	$scope.ec = true
+;			    	    })
+		    	    .error(function(data){
+		    	    	$scope.error = 'unable to get the poneys';
+		    	    });
+		    	} 
       // Crée la page permettant d'ajouter une formation
       $scope.ajoutFormation = function(){
         $location.path("/formation/nouveau"); 
@@ -78,8 +161,8 @@
   );
 
   app.controller('FormationDetailsController', 
-    ['$scope', '$routeParams','$http', '$location','$filter', 'formationsFactory',
-    function($scope, $routeParams, $http, $location,$filter, formationsFactory){  
+    ['$scope', '$stateParams','$http', '$location','$filter', 'formationsFactory',
+    function($scope, $stateParams, $http, $location,$filter, formationsFactory){  
 
     		  $http.get('http://localhost:8090/getDomaine/DIPLOME').
     		    success(function(data, status, headers, config) {
