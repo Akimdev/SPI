7132243c53ed2bb404d8de45dfd1f07eec4eec74
Youssef BRAHMI <brahmi.youssef.p@gmail.com>
2016-03-21 16:44:26 +0100

    ihm evaluation

--- src/main/resources/client/js/controllers/ConfigEvaluationController.js
+++ src/main/resources/client/js/controllers/ConfigEvaluationController.js
@@ -33,12 +33,84 @@
 	  };
    }]);
 
-  app.controller('ConfigEvaluationController', ['$scope', '$routeParams', '$location', '$filter', 'configEvalFactory',
-		    function($scope, $routeParams, $location,$filter, configEvalFactory){ 
+  app.controller('ConfigEvaluationController', ['$scope', '$stateParams', '$location', '$filter', 'configEvalFactory',
+		    function($scope, $stateParams, $location,$filter, configEvalFactory){ 
 	  
+	  	$scope.rubriques = [];
+	  
+	  	var promise = configEvalFactory.get($stateParams.id);
+	  	promise.success(function(data){
+	  		console.log(data);
+	  		$scope.eval = data;
+	  	});
+	  	
 		$scope.cancel = function(){
 			history.back();
 		}
+		
+		$scope.addRubrique = function(){
+			var rubrique = {
+					'idRubrique' : 1,
+					'designation' : "designation",
+					'questions' : []
+			}
+			$scope.rubriques.push(rubrique); 
+		}
+		
+		$scope.addQuestion = function(id){
+			var rubrique = $scope.getRubriqueById(id);
+			if(rubrique[0] != null)
+				rubrique[0].questions.push({'idQuestion' : 5, 'intitule': "Some question", qualificatif : {"minimal" : "Faible", "maximal" : "Excellent"}});
+		}
+		
+		$scope.getRubriqueById = function(id){
+			for(var i = 0; i < $scope.rubriques.length; i++){
+				if($scope.rubriques[i].idRubrique === id)
+					return $scope.rubriques;
+			}
+			return null;
+		}
+		
+		$scope.removeQuestion = function(idRubrique, idQuestion){	
+			var indexQues = -1;		
+			var comArr = $scope.getRubriqueById(idRubrique)[0].questions;
+			for( var i = 0; i < comArr.length; i++ ) {
+				console.log("current : ", comArr[i].idQuestion);
+				if( comArr[i].idQuestion === idQuestion ) {
+					indexQues = i;
+					break;
+				}
+			}
+			if( indexQues === -1 ) {
+				alert( "Something gone wrong" );
+			}
+			
+			var indexRub = -1;
+			for(var i = 0; i < $scope.rubriques.length; i++){
+				if($scope.rubriques[i].idRubrique === idRubrique)
+					indexRub = i;
+			}
+
+			$scope.rubriques[indexRub].questions.splice(indexQues, 1);		
+		};
+		
+		$scope.removeRubrique = function(idRubrique){	
+			var indexRub = -1;		
+			var comArr = $scope.getRubriqueById(idRubrique);
+			for( var i = 0; i < comArr.length; i++ ) {
+				console.log("current : ", comArr[i].idRubrique);
+				if( comArr[i].idRubrique === idRubrique ) {
+					indexRub = i;
+					break;
+				}
+			}
+			if( indexRub === -1 ) {
+				alert( "Something gone wrong" );
+			}
+
+			$scope.rubriques.splice(indexRub, 1);		
+		};
   }]);
+
  
 }).call(this);
